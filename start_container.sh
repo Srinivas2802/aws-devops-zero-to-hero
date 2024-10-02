41e39e85d46bba6ca8caa226ee862e00b7971878
Srinivas2802 <91659129+Srinivas2802@users.noreply.github.com>
2024-10-03 02:53:50 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull srinivasv2802/codedeploy:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 srinivasv2802/codedeploy:latest
